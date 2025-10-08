9206fc2851580b848e7142b96b2bc9e9a8bee008
ardysafian <kang_garooru@live.com>
2025-10-08 22:13:19 +0800

    day3

---
+++ conditionalLogic.js
@@ -0,0 +1,19 @@
+let x = 7;
+
+console.log(x === 7);
+
+// ==Checks value only
+// ===Checks both value and data type
+
+//>,< Larger than, smaller than
+// >=, <= Larger than or equals to, smaller than or equals to
+
+// OPERATORS
+
+// COMPARISON OPERATORS
+
+if (x > 5) {
+  console.log("X is larger than 5");
+} else {
+  console.log("X is not larger than 5");
+}
